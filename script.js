0bfc3f40f19bd85c82f5554a07e1ffb4cc25b483
Margulis162 <markmargulis@proton.me>
2023-09-03 11:13:44 -0400

    variables

---
+++ script.js
@@ -0,0 +1,7 @@
+// ||variables
+const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
+
+// ||f(x)
