5929649a31297f54680724581fd60eb9d72564cf
Margulis162 <markmargulis@proton.me>
2023-09-03 11:39:35 -0400

    event listener

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const button = document.querySelector("#set-alarm");
 const output = document.querySelector("#output");
 
 // ||f(x)
+button.addEventListener("click", async() => {
+    try{
+        const message = await alarm(name.value, delay.value);
+        output.textContent = message;
+    } catch (err) {
+        output.textContent = `Couldn't set alarm: ${err}`;
+    }
+});
