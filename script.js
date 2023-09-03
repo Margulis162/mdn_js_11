6d2092353acf2db5bed2d8719e719da563de7d79
Margulis162 <markmargulis@proton.me>
2023-09-03 18:51:08 -0400

    full js

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 // ||variables
-const name = document.querySelector("#name");
+const name1 = document.querySelector("#name");
 const delay = document.querySelector("#delay");
 const button = document.querySelector("#set-alarm");
 const output = document.querySelector("#output");
 
 // ||f(x)
-button.addEventListener("click", async() => {
-    try{
-        const message = await alarm(name.value, delay.value);
-        output.textContent = message;
-    } catch (err) {
-        output.textContent = `Couldn't set alarm: ${err}`;
-    }
-});
+function alarm(person, delay) {
+    return new Promise((resolve, rejected) => {
+        if(delay < 0){
+            throw new Error("Alarm delay must be positive");
+        }
+        setTimeout(() => {
+            resolve(`Wake up, ${person}`)
+        }, delay)
+    })
+}
+
+
+
+            //    event listenr 
+button.addEventListener("click", async function () {
+        try {
+            console.log(name1.value, delay.value);
+            const message = await alarm(name1.value, delay.value);
+            output.textContent = message;
+        } catch (err) {
+            output.textContent = `Couldn't set alarm: ${err}`;
+        }
+    });
